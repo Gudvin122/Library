dcd2d902edcbbb034a418a5b5a7c507fd5b4522a
Gudvin122 <skripchukg@gmail.com>
2026-06-25 16:06:23 +0700

    Вывел логику приложения в отдельный класс

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,61 +1,7 @@
 package org.example;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Optional;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        List<Student> students;
-        try (InputStream is = Main.class.getClassLoader().getResourceAsStream("students.json")) {
-            if (is == null) {
-                throw new RuntimeException("students.json not found");
-            }
-            students = objectMapper.readValue(
-                    is,
-                    new TypeReference<List<Student>>() {
-                    }
-            );
-        }
-
-        students.stream()
-                .map(Student::getName)
-                .forEach(System.out::println);
-
-        students.stream()
-                .map(Student::getBooks)
-                .forEach(System.out::println);
-
-        students.stream()
-                .flatMap(student -> student.getBooks().stream())
-                .map(Book::getTitle)
-                .forEach(System.out::println);
-
-        students.stream()
-                .flatMap(student -> student.getBooks().stream())
-                .sorted(java.util.Comparator.comparingInt(Book::getPages))
-                .distinct()
-                .filter(book -> book.getYear() > 2000)
-                .limit(3)
-                .map(Book::getYear)
-                .forEach(System.out::println);
-
-        Optional<Integer> year = students.stream()
-                .flatMap(student -> student.getBooks().stream())
-                .filter(book -> "Игра престолов".equals(book.getTitle()))
-                .map(Book::getYear)
-                .findFirst();
-
-        year.ifPresentOrElse(
-                y -> System.out.println("Год выпуска найденной книги: " + y),
-                () -> System.out.println("Такая книга отсутствует")
-        );
+    public static void main(String[] args) {
+        new JsonParser().parse();
     }
 }
-
